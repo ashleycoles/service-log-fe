63b192bfe76526842169b3f1f4f45d1ea80ff863
Ash Coles <ashley@acoles.co.uk>
2024-07-04 10:28:56 +0100

    Refactoring to log store

--- src/lib/components/LogBook.svelte
+++ src/lib/components/LogBook.svelte
@@ -1,11 +1,12 @@
 <script>
-    import { getLogs } from "../requests/logRequests"
     import LogBookSummary from "./LogBookSummary.svelte"
     import SingleLogModal from "./SingleLogModal.svelte"
     import TypeDropdown from "./TypeDropdown.svelte"
+    import {logs} from "../stores/logs"
 
     let selectedType
-    $: response = getLogs(selectedType ?? null)
+
+    $: logs.refresh(selectedType)
 
     let logToDisplayId
 </script>
@@ -14,17 +15,14 @@
     <div class="mb-5">
         <TypeDropdown on:typeChange={e => selectedType = e.detail.typeId}/>
     </div>
-    {#await response}
-        Loading...
-    {:then logs} 
-        {#each logs.data as log (log.id)}
-            <LogBookSummary id={log.id} title={log.title} type={log.type.name} date={log.date} on:displayLog={e => logToDisplayId = e.detail.id}/>
-        {/each}
 
-        {#if logs.data.length === 0}
-            <p>No logs found</p>
-        {/if}
-    {/await}
+    {#each $logs as log (log.id)}
+        <LogBookSummary id={log.id} title={log.title} type={log.type.name} date={log.date} on:displayLog={e => logToDisplayId = e.detail.id}/>
+    {/each}
+
+    {#if logs.length === 0}
+        <p>No logs found</p>
+    {/if}
 
     {#if logToDisplayId}
         <SingleLogModal id={logToDisplayId} />
---
+++ src/lib/stores/logs.js
@@ -0,0 +1,28 @@
+import { readable } from "svelte/store";
+import { getLogs } from "../requests/logRequests";
+
+function createLogsStore() {
+    let setStore;
+
+    const { subscribe } = readable([], set => {
+        setStore = set;
+        getData();
+        return () => {};
+    });
+
+    const getData = async (type = null) => {
+        try {
+            const data = await getLogs(type);
+            setStore(data.data);
+        } catch (error) {
+            console.error("Failed to fetch types:", error);
+        }
+    };
+
+    return {
+        subscribe,
+        refresh: getData 
+    };
+}
+
+export const logs = createLogsStore();
